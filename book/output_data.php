8d281be55cb577648df2dd5385f0410045600ff6
Masaaki Todoroki <xxx.good.luck.my.love.xxx@gmail.com>
2018-02-08 07:22:37 +0900

    コード修正

--- book/output_data.php
+++ book/output_data.php
@@ -2,20 +2,22 @@
   //セッション開始
   session_start();
 
-	//関数定義ファイル読み込み
-	include("../include/functions.php");
+  //関数定義ファイル読み込み
+  include("../include/functions.php");
 
   //セッション変数として渡されているuserIdを変数$userIdに格納
   $userId = $_SESSION['userId'];
 
-	//DB CONNECTION関数実行
-	$pdo = dbConnection();
+  //DB CONNECTION関数実行
+  $pdo = dbConnection();
 
   //実行したいSQL文を変数$aqlに格納
   $sql = 'SELECT * FROM book_table WHERE userId = :userId';
 
   //実行したいSQL文をセット
   $stmt = $pdo -> prepare($sql);
+
+  //bindvalue
   $stmt -> bindValue(':userId', $userId, PDO::PARAM_STR);
 
   //実際に実行　→　それを変数$flagに格納
@@ -24,8 +26,8 @@
   //失敗　→　エラーメッセージ表示
   if($flag == false){
     
-		//SQL ERROR関数実行
-		queryError($stmt);
+    //SQL ERROR関数実行
+    queryError($stmt);
 		
   }
   //成功　→　以下のDOMを実行
@@ -39,7 +41,7 @@
       <meta name="viewport" content="width=device-width,initial-scale=1">
       <meta name="format-detection" content="telephone=no">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Book Memo</title>
+      <title>登録書籍</title>
       <link rel="stylesheet" href="../lib/css/book/icomoon/icomoon_style.css">
       <link rel="stylesheet" href="../lib/css/book/book2.css">
       <script src="../lib/js/jquery-3.2.0.min.js"></script>
@@ -54,19 +56,19 @@
               //実行して返ってきたデータを変数$resultに格納　書籍データの数だけwhileで回して表示
               while( $result = $stmt -> fetch(PDO::FETCH_ASSOC) ){
             ?>
-						<li>
-							<div class="list_img">
-								<img src="<?php echo $result['url']; ?>">
-							</div>
-							<div class="list_t">
-								<h3 class="title"><?php echo $result['title']; ?></h3>
-								<p class="text"><?php echo $result['comment']; ?></p>
-							</div>
-							<div class="delete-btn">
-								<a href="delete.php?id='<?php echo $result["id"] ?>'">
-									<img src="../lib/img/user/icon_delete.png" class="icon_delete">
-								</a>
-							</div>
+              <li>
+                <div class="list_img">
+                  <img src="<?php echo $result['url']; ?>">
+                </div>
+                <div class="list_t">
+                  <h3 class="title"><?php echo $result['title']; ?></h3>
+                  <p class="text"><?php echo $result['comment']; ?></p>
+                </div>
+                <div class="delete-btn">
+                    <a href="delete_data.php?id='<?php echo $result["id"] ?>'">
+                      <img src="../lib/img/user/icon_delete.png" class="icon_delete">
+                    </a>
+                </div>
               </li>
             <?php
               }
