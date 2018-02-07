b90950b67e0e8bd0882c941dbe00e6b4b8d0a763
Masaaki Todoroki <xxx.good.luck.my.love.xxx@gmail.com>
2018-02-08 07:35:18 +0900

    コメントアウト記入

--- book/delete_data.php
+++ book/delete_data.php
@@ -19,36 +19,27 @@
   //prepareメソッドで定義
   $stmt = $pdo -> prepare($sql);
 
-  //bindValueを
+  //bindValue
   $stmt -> bindValue(":userId", $userId, PDO::PARAM_STR);
   $stmt -> bindValue(":bookId", $bookId, PDO::PARAM_STR);
 
   //実行
   $flag = $stmt -> execute();
-
-  //３．データ表示
-  $view="";
-
+  
+  //実行が失敗したら
   if($flag == false){
 
-    error_db_Info($stmt);
+    //queryError関数実行
+    queryError($stmt);
 
-  } 
+  }
+  //実行が成功したら
   else {
 
-    //Selectデータの数だけ自動でループしてくれる ->は中の〜関数を使うという意味 :〜はバインド関数
-
-    //    while( $result = $stmt -> fetch(PDO::FETCH_ASSOC)){
-    //      $view .= '<p>';
-    //        $view .= '<a href="detail.php?id='.$result["id"].'">'; 
-    //          $view .= $result["name"]."[".$result["indate"]."]";
-    //        $view .= '</a>';
-    //      $view .= '</p>';
-    //    }
-
-    //header ~ exitはワンセット
-    header("Location: select.php");
+    //ユーザーの一覧画面に戻す
+    header("Location: output_data.php");
     exit();
+    
   }
 
 
